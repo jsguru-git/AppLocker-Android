638491404bb5a8215bfbfd88b618c7f8fea789cf
asan <i.asan825.mail@gmail.com>
2017-11-27 09:30:41 -0800

    #1. modified the logic of LockService a little(added compareForHomeButtonfunction, etc). #2. overrided onBackPressd() and onPause() in LockActivity

--- app/src/main/java/com/app/locker/activity/LockActivity.java
+++ app/src/main/java/com/app/locker/activity/LockActivity.java
@@ -3,11 +3,29 @@ package com.app.locker.activity;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
 
 /**
  * Created by KBest on 11/24/2017.
  */
 
 public class LockActivity extends Activity {
+    @Override
+    public void onBackPressed() {
+        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);
+        return;
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);
+        return;
+    }
 }
--- app/src/main/java/com/app/locker/receiver/OnAlarmReceiver.java
+++ app/src/main/java/com/app/locker/receiver/OnAlarmReceiver.java
@@ -4,6 +4,7 @@ import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -33,80 +34,93 @@ public class OnAlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.e(Constants.TAG, "Alarm Service Receiver");
         mContext = context;
-        mContext.getPackageManager();
-        PackageManager pm = mContext.getPackageManager();
-        List<ApplicationInfo> apps = pm.getInstalledApplications(0);
 
-        String[] install = new String[apps.size()];
-        int count = 0;
+        if (Preference.getLockSetting(mContext)) {
+            PackageManager pm = mContext.getPackageManager();
+            List<ApplicationInfo> apps = pm.getInstalledApplications(0);
 
-        for(ApplicationInfo app : apps)
-        {
-            install[count] = app.packageName;
-            count++;
-        }
+            String[] install = new String[apps.size()];
+            int count = 0;
 
-//        get launcher package name
-        Intent launcher_intent = new Intent(Intent.ACTION_MAIN);
-        launcher_intent.addCategory(Intent.CATEGORY_HOME);
-        ResolveInfo defaultLauncher = mContext.getPackageManager().resolveActivity(launcher_intent, PackageManager.MATCH_DEFAULT_ONLY);
-        String deaultLauncherStr = defaultLauncher.activityInfo.packageName;
+            for (ApplicationInfo app : apps) {
+                install[count] = app.packageName;
+                count++;
+            }
 
-//        get sms package name
-        String sms_name = new String();
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-        {
-            sms_name = Telephony.Sms.getDefaultSmsPackage(mContext);
-        }
-        else {
-            Intent sms_intent = new Intent(Intent.ACTION_VIEW).addCategory(Intent.CATEGORY_DEFAULT).setType("vnd.android-dir/mms-sms");
-            final List<ResolveInfo> sms_resolveInfos = mContext.getPackageManager().queryIntentActivities(sms_intent, 0);
-            if (sms_resolveInfos != null && !sms_resolveInfos.isEmpty())
-                sms_name = sms_resolveInfos.get(0).activityInfo.packageName;
-        }
+            //        get launcher package name
+            Intent launcher_intent = new Intent(Intent.ACTION_MAIN);
+            launcher_intent.addCategory(Intent.CATEGORY_HOME);
+            ResolveInfo defaultLauncher = mContext.getPackageManager().resolveActivity(launcher_intent, PackageManager.MATCH_DEFAULT_ONLY);
+            String deaultLauncherStr = defaultLauncher.activityInfo.packageName;
 
-//        remove launcher, sms and phone package
-        install = (String[]) ArrayUtils.removeElement(install, deaultLauncherStr);
-        install = (String[]) ArrayUtils.removeElement(install, sms_name);
-        install = (String[]) ArrayUtils.removeElement(install, "com.android.systemui");
+            //        get sms package name
+            String sms_name = new String();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                sms_name = Telephony.Sms.getDefaultSmsPackage(mContext);
+            } else {
+                Intent sms_intent = new Intent(Intent.ACTION_VIEW).addCategory(Intent.CATEGORY_DEFAULT).setType("vnd.android-dir/mms-sms");
+                final List<ResolveInfo> sms_resolveInfos = mContext.getPackageManager().queryIntentActivities(sms_intent, 0);
+                if (sms_resolveInfos != null && !sms_resolveInfos.isEmpty())
+                    sms_name = sms_resolveInfos.get(0).activityInfo.packageName;
+            }
 
-        activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        String[] activePackages;
+            //        remove launcher, sms and phone package
+            install = (String[]) ArrayUtils.removeElement(install, deaultLauncherStr);
+            install = (String[]) ArrayUtils.removeElement(install, sms_name);
+            install = (String[]) ArrayUtils.removeElement(install, "com.android.systemui");
 
-        if (Build.VERSION.SDK_INT > 20) {
-            activePackages = getActivePackages();
-        } else {
-            activePackages = getActivePackagesCompat();
-        }
+            activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            String[] activePackages;
 
-        for(int i = 0 ; i < activePackages.length ; i++)
-        {
-            Log.e(Constants.TAG, "Package Name : " + activePackages[i]);
-        }
+            if (Build.VERSION.SDK_INT > 20) {
+                activePackages = getActivePackages();
+            } else {
+                activePackages = getActivePackagesCompat();
+            }
 
-        Log.e(Constants.TAG, "My Package Name : " + mContext.getPackageName());
+            for (int i = 0; i < activePackages.length; i++) {
+                Log.e(Constants.TAG, "Package Name : " + activePackages[i]);
+            }
 
-        Intent pwdIntent = new Intent(context, LockActivity.class);
-        pwdIntent.setFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_CLEAR_TOP);
+            Log.e(Constants.TAG, "My Package Name : " + mContext.getPackageName());
 
-        if (activePackages != null) {
-            int length = activePackages.length;
-            int i = 0;
-            while (i < length) {
-                String activePackage = activePackages[i];
-                Log.e(Constants.TAG, activePackage);
-                for (int j=0;j<install.length;j++) {
-                    if(Preference.getLockSetting(mContext) && !activePackage.equalsIgnoreCase(mContext.getPackageName()) && activePackage.equalsIgnoreCase(install[j])) {
-                        Log.e("package name match", activePackage);
-                        pwdIntent.putExtra("Packagename", activePackage);
-                        this.mContext.startActivity(pwdIntent);
+            Intent pwdIntent = new Intent(context, LockActivity.class);
+            pwdIntent.setFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_CLEAR_TOP);
+
+            Intent intent1 = new Intent("android.intent.action.MAIN");
+            intent1.addCategory("android.intent.category.HOME");
+            compareForHomeButton(this.mContext.getPackageManager().resolveActivity(intent1, 65536).activityInfo.packageName, activePackages);
+
+            if (activePackages != null) {
+                int length = activePackages.length;
+                int i = 0;
+                while (i < length) {
+                    String activePackage = activePackages[i];
+                    Log.e(Constants.TAG, activePackage);
+                    for (int j = 0; j < install.length; j++) {
+                        if (!activePackage.equalsIgnoreCase(mContext.getPackageName()) && activePackage.equalsIgnoreCase(install[j]) && getpreferences("Lock").equalsIgnoreCase("False")) {
+                            Log.e("package name match", activePackage);
+                            SavePreferences("Lock", "Still");
+                            pwdIntent.putExtra("Packagename", activePackage);
+                            this.mContext.startActivity(pwdIntent);
+                        }
                     }
+                    i++;
                 }
-                i++;
             }
         }
     }
 
+
+    private void SavePreferences(String key, String value) {
+        SharedPreferences.Editor editor = this.mContext.getSharedPreferences("pref", 0).edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    private String getpreferences(String key) {
+        return this.mContext.getSharedPreferences("pref", 0).getString(key, "0");
+    }
 
     public String[] getActivePackagesCompat() {
         return new String[]{((ActivityManager.RunningTaskInfo) this.activityManager.getRunningTasks(1).get(0)).topActivity.getPackageName()};
@@ -122,4 +136,16 @@ public class OnAlarmReceiver extends BroadcastReceiver {
         return (String[]) activePackages.toArray(new String[activePackages.size()]);
     }
 
+    public void compareForHomeButton(String homePackagename, String[] activePackages) {
+        if (activePackages != null) {
+            for (String activePackage : activePackages) {
+                if (homePackagename.equals(activePackage)) {
+                    if (!getpreferences("Lock").equalsIgnoreCase("False")) {
+                        SavePreferences("Lock", "False");
+                    }
+                    Log.e("current home page set", "true");
+                }
+            }
+        }
+    }
 }
